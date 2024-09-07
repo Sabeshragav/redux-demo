9ce27a99b4b57a169e26194d5f2e38bfbec494ab
Sabeshragav <sabeshragav@gmail.com>
2024-09-07 21:21:05 +0530

    saved

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { store } from "./data/store";
+import { Provider } from "react-redux";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
